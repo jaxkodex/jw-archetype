ba0fd77e13029acc8a4c44bbbad68720fb035bb2
Jax Kodex <jkodex@gmail.com>
2015-09-12 09:27:28 -0500

    configured to set the package on generated clases

--- src/main/resources/archetype-resources/src/main/java/controllers/IndexController.java
+++ src/main/resources/archetype-resources/src/main/java/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package org.kodex.web.controllers;
+package ${package}.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
